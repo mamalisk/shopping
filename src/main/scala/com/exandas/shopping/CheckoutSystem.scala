5318bb414693616ad56f62ff1fde0152dc99d18e
kostas mamalis <kostas@Konstantinoss-Mac-Pro.local>
2015-05-28 13:05:19 +0100

    adding offers

--- src/main/scala/com/exandas/shopping/CheckoutSystem.scala
+++ src/main/scala/com/exandas/shopping/CheckoutSystem.scala
@@ -1,10 +1,22 @@
 package com.exandas.shopping
 
+case class Offer(fruit:String, discount : (Int,Int)){
+  if(discount._2 > discount._1) throw new RuntimeException("wrong discount")
+  require(discount._2 > discount._1, "wrong discount")
+  require(fruit.equalsIgnoreCase("apple") || fruit.equalsIgnoreCase("orange"), s"unknown fruit $fruit")
+
+}
+
 case class CheckoutSystem(fruits:List[Fruit]) {
-    def totalCost : String = {
+
+   var offers : List[Offer] = List[Offer]()
+
+   def totalCost : String = {
        val result = fruits.foldRight(0d)((r,c) => r.priceInCurrency.getActual + c)
        s"£$result"
     }
+
+
 }
 
 object CheckoutSystem {
@@ -12,6 +24,6 @@ object CheckoutSystem {
     import Conversions._
     var actualFruitList : List[Fruit] = List[Fruit]()
     fruitsList.foreach( fruitString => actualFruitList = actualFruitList.::(fruitString) )
-    return new CheckoutSystem(actualFruitList)
+    new CheckoutSystem(actualFruitList)
   }
 }
